b64c4549fcc022b07a0e45b84170e4b96deca801
Ivan Gladkikh <ivangladkikh3@gmail.com>
2020-12-02 16:54:26 +0500

    (feat): Добавил сборку .sass
    Также добавил autoprefixer и настроил
    file watcher в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp');
-const gulp_sass = require('gulp-sass');
+const sass = require('gulp-sass');
 const babelify = require('babelify');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const rename = require('gulp-rename');
+const prefix = require('gulp-autoprefixer');
 
 gulp.task('bundle-js', () => {
     return browserify({entries: "src/index.js", extensions: ['.js'], debug: true})
@@ -16,9 +17,23 @@ gulp.task('bundle-js', () => {
         .pipe(gulp.dest("dist"));
 });
 
+gulp.task('gulp-sass', () => {
+    return gulp.src('src/index.sass')
+        .pipe(sass())
+        .pipe(prefix())
+        .pipe(rename({
+            suffix: '.bundle'
+        }))
+        .pipe(gulp.dest('dist/'))
+})
+
 gulp.task('build', () => {
     return gulp.src('src/*.html')
         .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', gulp.series('bundle-js', 'build'));
+gulp.task("watch", () => {
+    gulp.watch(["src/pages/**/*", "src/index.js", 'src/index.html'], gulp.series('build', "gulp-sass", "bundle-js"));
+});
+
+gulp.task('default', gulp.series('bundle-js', 'gulp-sass', 'build', 'watch'));
