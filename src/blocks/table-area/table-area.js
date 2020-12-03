73150f10b50ee9742f10ba1ab647bee5d74c7cbd
Ivan Gladkikh <ivangladkikh3@gmail.com>
2020-12-03 20:18:35 +0500

    (add): Подключил иконки bootstrap

--- src/blocks/table-area/table-area.js
+++ src/blocks/table-area/table-area.js
@@ -6,6 +6,7 @@ import mapStateToProps from "../../store/mapStateToProps";
 //______ Blocks ________________
 import Heading from "../heading/heading";
 import ButtonSection from "./elements/button-section";
+import BtstrapIcon from "../btstrap-icon/btstrap-icon";
 
 class TableArea extends React.Component {
     render() {
@@ -25,7 +26,11 @@ class TableArea extends React.Component {
             <div className={'table-area'}>
                 <Heading className={'text_color-dark'}>{area_name}</Heading>
                 <ButtonSection>
-                    <button type={'button'} className={'btn btn-success button button_size-small'}>Добавить запись</button>
+                    <button type={'button'} className={'btn btn-success button button_size-small'}>
+                        <BtstrapIcon data={'bi-bookmark-plus'}
+                                     className={'bi-bookmark-plus button__btstrap-icon btstrap-icon_size-14 btstrap-icon_color-white'}/>
+                        Добавить запись
+                    </button>
                 </ButtonSection>
             </div>
         )
