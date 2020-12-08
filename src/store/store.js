ad29c7607ee4968ce2df801b22d4252d38954692
Ivan Gladkikh <ivangladkikh3@gmail.com>
2020-12-08 15:21:24 +0500

    (fix): Настроил работу redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,15 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import reducer from "./reducer";
 import initialState from "./initialState";
+import thunk from "redux-thunk";
 
-const store = createStore(reducer, initialState);
+const store = createStore(
+    reducer,
+    initialState,
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+);
 
 export default store;
